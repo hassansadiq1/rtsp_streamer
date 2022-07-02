38c89533a802a91532cbd448ffa23825fafeeee2
root <hassansadiq2k15@gmail.com>
2022-07-02 20:52:02 +0000

    initial working commit

---
+++ main.py
@@ -0,0 +1,270 @@
+import sys
+
+sys.path.append('/opt/nvidia/deepstream/deepstream/lib')
+import gi
+
+gi.require_version('Gst', '1.0')
+gi.require_version('GstRtspServer', '1.0')
+from gi.repository import GObject, Gst, GstRtspServer
+from bus_call import bus_call
+from pipeline import Pipeline
+import pyds
+import json
+from datetime import datetime
+
+
+detector = Pipeline()
+dateTimeObj = datetime.now()
+file_name = str(dateTimeObj) + '.json'
+
+labels = {0:"person", 1: "bicycle", 2: "car", 3: "motorbike", 4: "aeroplane", 5: "bus",
+            6: "train", 7: "truck", 8: "boat", 9: "traffic light", 10: "fire hydrant",
+            11: "stop sign", 12: "parking meter", 13: "bench", 14: "bird", 15: "cat",
+            16: "dog", 17: "horse", 18: "sheep", 19: "cow", 20: "elephant", 21: "bear",
+            22: "zebra", 23: "giraffe", 24: "backpack", 25: "umbrella", 26: "handbag", 27: "tie",
+            28: "suitcase", 29: "frisbee", 30: "skis", 31: "snowboard", 32: "sports ball", 33: "kite",
+            34: "baseball bat", 35: "baseball glove", 36: "skateboard", 37: "surfboard", 38: "tennis racket", 39: "bottle",
+            40: "wine glass", 41: "cup", 42: "fork", 43: "knife", 44: "spoon", 45: "bowl", 46: "banana",
+            47: "apple", 48: "sandwich", 49: "orange", 50: "broccoli", 51: "carrot", 52: "hot dog",
+            53: "pizza", 54: "donut", 55: "cake", 56: "chair", 57: "sofa", 58: "pottedplant",
+            59: "bed", 60: "diningtable", 61: "toilet", 62: "tvmonitor", 63: "laptop", 64: "mouse",
+            65: "remote", 66: "keyboard", 67: "cell phone", 68: "microwave", 69: "oven", 70: "toaster",
+            71: "sink", 72: "refrigerator", 73: "book", 74: "clock", 75: "vase", 76: "scissors",
+            77: "teddy bear", 78: "hair drier", 79: "toothbrush"}
+
+
+def osd_sink_pad_buffer_probe(pad,info,u_data):
+
+    global file_name
+
+    gst_buffer = info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer ")
+        return
+
+    # Retrieve batch metadata from the gst_buffer
+    # Note that pyds.gst_buffer_get_nvds_batch_meta() expects the
+    # C address of gst_buffer as input, which is obtained with hash(gst_buffer)
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            # Note that l_frame.data needs a cast to pyds.NvDsFrameMeta
+            # The casting is done by pyds.glist_get_nvds_frame_meta()
+            # The casting also keeps ownership of the underlying memory
+            # in the C code, so the Python garbage collector will leave
+            # it alone.
+            #frame_meta = pyds.glist_get_nvds_frame_meta(l_frame.data)
+            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        frame_number=frame_meta.frame_num
+        l_obj=frame_meta.obj_meta_list
+
+        while l_obj is not None:
+            try:
+                # Casting l_obj.data to pyds.NvDsObjectMeta
+                #obj_meta=pyds.glist_get_nvds_object_meta(l_obj.data)
+                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+
+            data = {
+                "frame_number": frame_number,
+                "class" : labels[obj_meta.class_id],
+                "left" : int(obj_meta.rect_params.left),
+                "top" : int(obj_meta.rect_params.top),
+                "width" : int(obj_meta.rect_params.width),
+                "height" : int(obj_meta.rect_params.height)
+            }
+            json_object = json.dumps(data, indent = 4)
+            # Writing to sample.json
+            with open(file_name, "a") as outfile:
+                outfile.write(json_object)
+                outfile.write(",\n")
+
+            # print(json_object)
+            try: 
+                l_obj=l_obj.next
+            except StopIteration:
+                break
+
+        try:
+            l_frame=l_frame.next
+        except StopIteration:
+            break
+			
+    return Gst.PadProbeReturn.OK	
+
+
+def cb_newpad(decodebin, decoder_src_pad, data):
+    print("In cb_newpad\n")
+    caps = decoder_src_pad.get_current_caps()
+    gststruct = caps.get_structure(0)
+    gstname = gststruct.get_name()
+    source_bin = data
+    features = caps.get_features(0)
+
+    # Need to check if the pad created by the decodebin is for video and not
+    # audio.
+    print("gstname=", gstname)
+    if gstname.find("video") != -1:
+        # Link the decodebin pad only if decodebin has picked nvidia
+        # decoder plugin nvdec_*. We do this by checking if the pad caps contain
+        # NVMM memory features.
+        print("features=", features)
+        if features.contains("memory:NVMM"):
+            # Get the source bin ghost pad
+            bin_ghost_pad = source_bin.get_static_pad("src")
+            if not bin_ghost_pad.set_target(decoder_src_pad):
+                sys.stderr.write("Failed to link decoder src pad to source bin ghost pad\n")
+        else:
+            sys.stderr.write(" Error: Decodebin did not pick nvidia decoder plugin.\n")
+
+
+def decodebin_child_added(child_proxy, Object, name, user_data):
+    print("Decodebin child added:", name, "\n")
+    if name.find("decodebin") != -1:
+        Object.connect("child-added", decodebin_child_added, user_data)
+
+
+def create_source_bin(index, uri):
+    print("Creating source bin")
+
+    # Create a source GstBin to abstract this bin's content from the rest of the
+    # pipeline
+    bin_name = "source-bin-%02d" % index
+    print(bin_name)
+    nbin = Gst.Bin.new(bin_name)
+    if not nbin:
+        sys.stderr.write(" Unable to create source bin \n")
+
+    # Source element for reading from the uri.
+    # We will use decodebin and let it figure out the container format of the
+    # stream and the codec and plug the appropriate demux and decode plugins.
+    uri_decode_bin = Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
+    if not uri_decode_bin:
+        sys.stderr.write(" Unable to create uri decode bin \n")
+    # We set the input uri to the source element
+    uri_decode_bin.set_property("uri", uri)
+    # Connect to the "pad-added" signal of the decodebin which generates a
+    # callback once a new pad for raw data has beed created by the decodebin
+    uri_decode_bin.connect("pad-added", cb_newpad, nbin)
+    uri_decode_bin.connect("child-added", decodebin_child_added, nbin)
+
+    # We need to create a ghost pad for the source bin which will act as a proxy
+    # for the video decoder src pad. The ghost pad will not have a target right
+    # now. Once the decode bin creates the video decoder and generates the
+    # cb_newpad callback, we will set the ghost pad target to the video decoder
+    # src pad.
+    Gst.Bin.add(nbin, uri_decode_bin)
+    bin_pad = nbin.add_pad(Gst.GhostPad.new_no_target("src", Gst.PadDirection.SRC))
+    if not bin_pad:
+        sys.stderr.write(" Failed to add ghost pad in source bin \n")
+        return None
+    return nbin
+
+
+def main(args):
+    # Check input arguments
+    if len(args) < 3:
+        sys.stderr.write("Usage: %s <rtsp input address> <rtsp output port>\n" % args[0])
+        sys.exit(1)
+
+    # Standard GStreamer initialization
+    GObject.threads_init()
+    Gst.init(None)
+
+    # Create gstreamer elements */
+    # Create Pipeline element that will form a connection of other elements
+    print("Creating Pipeline \n ")
+    detector.createElements()
+    detector.Verify()
+    detector.Configure()
+    detector.set_tracker_properties()
+    detector.ConstructPipeline()
+
+    if not detector._pipeline:
+        sys.stderr.write(" Unable to create Pipeline \n")
+
+    print("Creating Source Bin")
+    for i in range(1):
+        print("Creating source_bin ", i, " \n ")
+        uri_name = args[i + 1]
+        if uri_name.find("rtsp://") == 0:
+            detector._streammux.set_property('live-source', 1)
+
+        source_bin = create_source_bin(i, uri_name)
+        if not source_bin:
+            sys.stderr.write("Unable to create source bin \n")
+        detector._pipeline.add(source_bin)
+
+        padname = "sink_%u" % i
+        sinkpad = detector._streammux.get_request_pad(padname)
+        if not sinkpad:
+            sys.stderr.write("Unable to create sink pad bin \n")
+        srcpad = source_bin.get_static_pad("src")
+        if not srcpad:
+            sys.stderr.write("Unable to create src pad bin \n")
+        srcpad.link(sinkpad)
+
+    # create an event loop and feed gstreamer bus mesages to it
+    detector._bus = detector._pipeline.get_bus()
+    detector._bus.add_signal_watch()
+    detector._bus.connect("message", bus_call, detector._loop)
+
+    osdsinkpad = detector._nvosd.get_static_pad("sink")
+    if not osdsinkpad:
+        sys.stderr.write(" Unable to get sink pad of nvosd \n")
+
+    osdsinkpad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
+
+    udpsink_port_num = 5400
+    sink = Gst.ElementFactory.make("udpsink", "udpsink")
+    if not sink:
+        sys.stderr.write(" Unable to create udpsink")
+
+    sink.set_property('host', '224.224.255.255')
+    sink.set_property('port', udpsink_port_num)
+    sink.set_property('async', False)
+    sink.set_property('sync', 0)
+    sink.set_property("qos", 0)
+
+    detector._pipeline.add(sink)
+    detector._rtppay.link(sink)
+
+    # Start streaming
+    rtsp_port_num = args[i + 2]
+
+    server = GstRtspServer.RTSPServer.new()
+    server.props.service = f"{rtsp_port_num}"
+    server.attach(None)
+
+    factory = GstRtspServer.RTSPMediaFactory.new()
+    factory.set_launch(
+        f"( udpsrc name=pay0 port={udpsink_port_num} buffer-size=524288 caps=\"application/x-rtp, media=video, clock-rate=90000, encoding-name=(string){detector.ENCODER_CODEC}, payload=96 \" )"
+    )
+    factory.set_shared(True)
+    server.get_mount_points().add_factory("/gst_test", factory)
+
+    print(f"\n ***Launched RTSP Streaming at rtsp://localhost:{rtsp_port_num}/gst_test ***\n\n")
+
+    # List the sources
+    print("Now playing...")
+    print(args[1])
+
+    print("Starting pipeline \n")
+    # start play back and listed to events		
+    detector._pipeline.set_state(Gst.State.PLAYING)
+    try:
+        detector._loop.run()
+    except:
+        print("error/n/n/n")
+    # cleanup
+    print("Exiting app\n")
+    detector._pipeline.set_state(Gst.State.NULL)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
---
+++ pipeline.py
@@ -0,0 +1,179 @@
+import sys
+
+sys.path.append('/opt/nvidia/deepstream/deepstream/lib')
+import gi
+import configparser
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst
+import pyds
+
+
+class Pipeline:
+    def __init__(self):
+
+        self._MAX_DISPLAY_LEN = 64
+        self._MUXER_OUTPUT_WIDTH = 1920
+        self._MUXER_OUTPUT_HEIGHT = 1080
+        self._GST_CAPS_FEATURES_NVMM = "memory:NVMM"
+        self._MAX_ELEMENTS_IN_DISPLAY_META = 16
+        self._BITRATE = 4000000
+        self.ENCODER_CODEC = 'H264'
+
+        self._PGIE_CONFIG_FILE = "./configs/pgie_config.txt"
+        self._TRACKER_CONFIG_FILE = "./configs/tracker_config.txt"
+
+        self._loop = None
+        self._pipeline = self._streammux = self._sink = None
+        self._pgie = self._nvosd = None
+        self._nvvideoconvert1 = self._nvvideoconvert2 = None
+        self._nvvideoconvert3 = self._nvvideoconvert4 = None
+        self._nvtracker = None
+        self._fakesink = None
+        self._capsfilter1 = self._capsfilter2 = self._capsfilter3 = None
+        self._tee = self._encoder = self._rtppay = self._muxer = self._filesink = None
+        self._que1 = self._que2 = self._que3 = self._que4 = None
+
+        self._bus = None
+        self._bus_watch_id = None
+        self._osd_src_pad = None
+
+        self._pgie_batch_size = 1
+
+    def createElements(self):
+
+        self._pipeline = Gst.Pipeline()
+        self._loop = GObject.MainLoop()
+
+        # Create nvstreammux instance to form batches from one or more sources.
+        self._streammux = Gst.ElementFactory.make("nvstreammux", "Stream-muxer")
+
+        # Use nvinfer to infer on batched frame.
+        self._pgie = Gst.ElementFactory.make("nvinfer", "primary-nvinference-engine")
+
+        # We need to have a tracker to track the identified objects */
+        self._nvtracker = Gst.ElementFactory.make("nvtracker", "tracker")
+
+        # Use convertor to convert from NV12 to RGBA as required by nvosd */
+        self._nvvideoconvert1 = Gst.ElementFactory.make("nvvideoconvert", "nvvideo-converter1")
+        self._nvvideoconvert2 = Gst.ElementFactory.make("nvvideoconvert", "nvvideo-converter2")
+        self._nvvideoconvert3 = Gst.ElementFactory.make("nvvideoconvert", "nvvideo-converter3")
+        self._nvvideoconvert4 = Gst.ElementFactory.make("nvvideoconvert", "nvvideo-converter4")
+
+        # Create OSD to draw on the converted RGBA buffer */
+        self._nvosd = Gst.ElementFactory.make("nvdsosd", "nv-onscreendisplay")
+
+        self._fakesink = Gst.ElementFactory.make("fakesink", "fake-sink1")
+
+        # encoder elements
+        if self.ENCODER_CODEC == "H264":
+            print("Creating H264 Encoder \n ")
+            self._encoder = Gst.ElementFactory.make("nvv4l2h264enc", "encoder-h264")
+            self._rtppay = Gst.ElementFactory.make("rtph264pay", "rtph264-pay")
+        else:
+            print("Creating H265 Encoder \n ")
+            self._encoder = Gst.ElementFactory.make("nvv4l2h265enc", "encoder-h265")
+            self._rtppay = Gst.ElementFactory.make("rtph265pay", "rtph265-pay")
+
+        # queue elements
+        self._que1 = Gst.ElementFactory.make("queue", "queue-1")
+        self._que2 = Gst.ElementFactory.make("queue", "queue-2")
+        self._que3 = Gst.ElementFactory.make("queue", "queue-3")
+
+        # capsfilter
+        self._capsfilter1 = Gst.ElementFactory.make("capsfilter", "capsfilter-1")
+        self._capsfilter2 = Gst.ElementFactory.make("capsfilter", "capsfilter-2")
+        self._capsfilter3 = Gst.ElementFactory.make("capsfilter", "capsfilter-3")
+
+    def Verify(self):
+        if not self._pipeline or not self._streammux or not self._fakesink:
+            sys.stderr.write("Initial elements could not be created. Exiting.\n")
+            exit(-1)
+
+        if (not self._pgie or not self._nvtracker  or not self._nvosd):
+            sys.stderr.write("Pipeline elements could not be created. Exiting.\n")
+            exit(-1)
+        
+        if not self._nvvideoconvert1 or not self._nvvideoconvert2 or not self._nvvideoconvert3 or not self._nvvideoconvert4:
+            sys.stderr.write("Nvvideoconvert elements could not be created. Exiting.\n")
+            exit(-1)
+
+
+        if not self._encoder or not self._rtppay:
+            sys.stderr.write("Encoder elements could not be created. Exiting.\n")
+            exit(-1)
+
+        if (not self._que1 or not self._que2 or not self._que3 or
+                not self._capsfilter1 or not self._capsfilter2 or not self._capsfilter3):
+            sys.stderr.write("queue or capsfilter elements could not be created. Exiting.\n")
+            exit(-1)
+
+    def Configure(self):
+
+        self._streammux.set_property("batch-size", self._pgie_batch_size)
+        self._streammux.set_property("width", self._MUXER_OUTPUT_WIDTH)
+        self._streammux.set_property("height", self._MUXER_OUTPUT_HEIGHT)
+
+        self._pgie.set_property("config-file-path", self._PGIE_CONFIG_FILE)
+
+        # I420 required for encoder
+        caps1 = Gst.Caps.from_string("video/x-raw(memory:NVMM), format=I420")
+        self._capsfilter1.set_property("caps", caps1)
+
+        self._encoder.set_property('bitrate', self._BITRATE)
+
+    def set_tracker_properties(self):
+        # Set properties of tracker
+        config = configparser.ConfigParser()
+        config.read(self._TRACKER_CONFIG_FILE)
+        config.sections()
+
+        for key in config['tracker']:
+            if key == 'tracker-width':
+                tracker_width = config.getint('tracker', key)
+                self._nvtracker.set_property('tracker-width', tracker_width)
+            if key == 'tracker-height':
+                tracker_height = config.getint('tracker', key)
+                self._nvtracker.set_property('tracker-height', tracker_height)
+            if key == 'gpu-id':
+                tracker_gpu_id = config.getint('tracker', key)
+                self._nvtracker.set_property('gpu_id', tracker_gpu_id)
+            if key == 'll-lib-file':
+                tracker_ll_lib_file = config.get('tracker', key)
+                self._nvtracker.set_property('ll-lib-file', tracker_ll_lib_file)
+            if key == 'll-config-file':
+                tracker_ll_config_file = config.get('tracker', key)
+                self._nvtracker.set_property('ll-config-file', tracker_ll_config_file)
+            if key == 'enable-batch-process':
+                tracker_enable_batch_process = config.getint('tracker', key)
+                self._nvtracker.set_property('enable_batch_process', tracker_enable_batch_process)
+            if key == 'enable-past-frame':
+                tracker_enable_past_frame = config.getint('tracker', key)
+                self._nvtracker.set_property('enable_past_frame', tracker_enable_past_frame)
+
+    def ConstructPipeline(self):
+        self._pipeline.add(self._streammux)
+        self._pipeline.add(self._pgie)
+        self._pipeline.add(self._nvtracker)
+        self._pipeline.add(self._nvvideoconvert1)
+        self._pipeline.add(self._nvvideoconvert2)
+        self._pipeline.add(self._nvosd)
+        self._pipeline.add(self._capsfilter1)
+        self._pipeline.add(self._encoder)
+        self._pipeline.add(self._rtppay)
+        self._pipeline.add(self._que1)
+        self._pipeline.add(self._que2)
+        self._pipeline.add(self._que3)
+        # self._pipeline.add(self._fakesink)
+
+        self._streammux.link(self._que1)
+        self._que1.link(self._pgie)
+        self._pgie.link(self._nvtracker)
+        self._nvtracker.link(self._que2)
+        self._que2.link(self._nvvideoconvert1)
+        self._nvvideoconvert1.link(self._nvosd)
+        self._nvosd.link(self._que3)
+
+        self._que3.link(self._nvvideoconvert2)
+        self._nvvideoconvert2.link(self._capsfilter1)
+        self._capsfilter1.link(self._encoder)
+        self._encoder.link(self._rtppay)
